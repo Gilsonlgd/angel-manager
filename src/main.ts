84b58bff63661bca9a2d194628cb694208f7b958
Gilsonlgd <garciadasilvagilson@gmail.com>
2024-09-22 17:47:18 -0300

    ✨ feat: Implement makeTemplate method for template processing

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,34 @@
-const hello = (name: string): string => {
-  return `Hello there, ${name}!`;
-};
+import { Liquid } from "liquidjs";
+import path from "path";
+import fs from "fs";
 
-export { hello };
+const engine = new Liquid();
+
+export const makeTemplate = (dirname: string): void => {
+  const rootDir = "components";
+  const templatePath = path.join(
+    dirname,
+    `architecture/${rootDir}/index.liquid`
+  );
+  const templateContent = fs.readFileSync(templatePath, "utf8");
+  const componentName = "MyComponent";
+
+  const data = {
+    componentName,
+  };
+
+  fs.mkdirSync(`${dirname}/${rootDir}/${componentName}`, { recursive: true });
+  engine
+    .parseAndRender(templateContent, data)
+    .then((output) => {
+      const outputPath = path.join(
+        dirname,
+        `${rootDir}/${componentName}/${componentName}.tsx`
+      );
+      fs.writeFileSync(outputPath, output);
+      console.log("Arquivo .tsx gerado com sucesso!");
+    })
+    .catch((err) => {
+      console.error("Erro ao gerar o arquivo .tsx:", err);
+    });
+};
